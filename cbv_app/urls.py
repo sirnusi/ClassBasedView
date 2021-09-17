25aee931346ae8582816e04eacb1a137f93ed7ca
sirnusi <davidsanusi360@gmail.com>
2021-09-17 12:17:02 +0100

    class commit

--- cbv_app/urls.py
+++ cbv_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, SchoolDetailView, SchoolListView
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('school_list/', SchoolListView.as_view(), name='list'),
+    path('school_detail/<int:pk>/', SchoolDetailView.as_view(), name='detail'),
 ]
