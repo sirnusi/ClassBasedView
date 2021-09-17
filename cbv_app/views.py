e84e9977a108ddcc56d2bdfe57fcd2cba0e04113
sirnusi <davidsanusi360@gmail.com>
2021-09-17 11:05:48 +0100

    class commit

--- cbv_app/views.py
+++ cbv_app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.http import HttpResponse
+from .models import School, Student
 # Create your views here.
 
-class HomeView(TemplateView):
+class Home(TemplateView):
     template_name = 'cbv_app/home.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['inject_me'] =  'BASIC INJECTION'
         return context
+
+class SchoolListView(ListView):
+    model = School
+
+class SchoolDetailView(DetailView):
+    model = School
+    
--- classBasedView/urls.py
+++ classBasedView/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from cbv_app import views
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name = 'index')
+    path('cbv_app/', include('cbv_app.urls'))
 ]
